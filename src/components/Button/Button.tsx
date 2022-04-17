f3fc1221432df0957d8e110e72145232dd677365
Lawrence Ikpebe <lawrenceikpebe@gmail.com>
2022-04-17 09:22:13 +0100

    add styled components

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
 
 interface ButtonProps {
   label: string;
 }
 
+const StyledButton = styled.button`
+  border: 1px dotted yellow;
+  background: tomato;
+  color: black;
+  font-weight: 500;
+`;
+
 const Button: React.FC<ButtonProps> = ({ label }) => {
-  return <button>{label}</button>;
+  return <StyledButton>{label}</StyledButton>;
 };
 
 export default Button;
